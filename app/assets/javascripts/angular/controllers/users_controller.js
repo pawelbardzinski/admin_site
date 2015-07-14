8d80c436193d0cf183604c7665c37376ce47fb06
Adam <a.kubicki1992@gmail.com>
2015-07-14 22:16:05 +0200

    users fetched

--- app/assets/javascripts/angular/controllers/users_controller.js
+++ app/assets/javascripts/angular/controllers/users_controller.js
@@ -1,4 +1,25 @@
 angular.module('app').controller('usersCtrl', ['$scope', function($scope) {
+$scope.usersFetched = false;
 
-console.log('hello');
+  $scope.getUsers = function() {
+    var User = Parse.Object.extend("User");
+    var user = new User();
+
+    user.fetch({
+      success: function(myObject) {
+        $scope.users = (myObject._serverData.results)
+        $scope.usersFetched = true;
+        $scope.$apply();
+        // The object was refreshed successfully.
+      },
+      error: function(myObject, error) {
+        $scope.usersFetched = 'error';
+        // The object was not refreshed successfully.
+        // error is a Parse.Error with an error code and message.
+      }
+    });
+  }
+
+  $scope.getUsers();
+
 }]);
