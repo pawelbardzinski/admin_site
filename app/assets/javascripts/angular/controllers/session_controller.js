ed727ba0d281d3fc56b6978e9f2976d96b7ad0c4
Adam <a.kubicki1992@gmail.com>
2015-07-14 22:52:43 +0200

    fixed notifices

--- app/assets/javascripts/angular/controllers/session_controller.js
+++ app/assets/javascripts/angular/controllers/session_controller.js
@@ -3,22 +3,22 @@ angular.module('app').controller('sessionCtrl', ['$scope', '$state', function($s
   $scope.disabledButton = false;
 
   $scope.signInUser = function() {
+    $scope.success = "";
     $scope.disabledButton = true;
     Parse.User.logIn($scope.user.username, $scope.user.password, {
       success: function(user) {
         $scope.disabledButton = false;
-        $scope.error = ""
+        $scope.alerts.error = ""
         $scope.user.currentUser = Parse.User.current();
-        $scope.success = "You are signed in as " + $scope.user.currentUser
         $state.forceReload();
+        $scope.alerts.success = "You have been signed in as " + $scope.user.currentUser.attributes.username
         $scope.$apply();
       },
       error: function(user, error) {
         $scope.disabledButton = false;
-        $scope.success = ""
-        $scope.error = "Invalid username or password"
+        $scope.alerts.success = ""
+        $scope.alerts.error = "Invalid username or password"
         $scope.$apply();
-
       }
     });
   }
