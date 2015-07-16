2f7e6803ec4898b9f3f17bd327213b8bc8cbec8b
Adam <a.kubicki1992@gmail.com>
2015-07-17 00:01:16 +0200

    added possibility to create hospitals

--- app/assets/javascripts/angular/controllers/facility_controller.js
+++ app/assets/javascripts/angular/controllers/facility_controller.js
@@ -6,27 +6,67 @@ angular.module('app').controller('facilityCtrl', ['$scope', function($scope) {
 
   $scope.getFacility = function() {
     var Facility = Parse.Object.extend("Facility");
-    var facility = new Facility();
+    var query = new Parse.Query("Facility");
 
-    facility.fetch({
-      success: function(myObject) {
-        $scope.facilities = (myObject._serverData.results)
+    query.find({
+      success: function(paramsFacilities) {
+        $scope.facilities = paramsFacilities;
+        _.each($scope.facilities, function(value, key) {
+          facilityQuery = value.relation('units').query()
+          facilityQuery.find({
+            success: function(results) {
+              value.unitAttributes = _.map(results, function(unit) {
+                return {
+                  unitId: unit.id,
+                  unitName: unit.attributes.name
+                }
+              })
+              $scope.$apply();
+            },
+            error: function(error) {
+              value.unitAttributes = []
+            }
+          });
+        })
         $scope.facilitiesFetched = true;
-        $scope.$apply();
         // The object was refreshed successfully.
       },
-      error: function(myObject, error) {
+      error: function(facilities, error) {
         $scope.usersFetched = 'error';
         // The object was not refreshed successfully.
         // error is a Parse.Error with an error code and message.
       }
+
     });
+  }
+
+  $scope.addNewFacility = function() {
+    var Facility = Parse.Object.extend("Facility");
+    var facility = new Facility();
+    facility.set("name", $scope.name)
+    facility.set("varianceReasons", $scope.varianceReasons)
+    facility.save(null, {
+      success: function(facility) {
+        $scope.facilities.push(facility)
+        $scope.alerts.success = "New Facility has been created"
+        $scope.$apply();
+      },
+      error: function(facility, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        $scope.alerts.error = error
+      }
+    });
+  }
+
+  $scope.unitNames = function(facility) {
+    return _.pluck(facility.unitAttributes, "unitName").join(', ')
   }
 
 
   $scope.$watch('user.currentUser', function(newVal, oldVal) {
     if ($scope.user.currentUser) {
-      $scope.getFacility();
+      // $scope.getFacility();
     }
   }, true);
 
