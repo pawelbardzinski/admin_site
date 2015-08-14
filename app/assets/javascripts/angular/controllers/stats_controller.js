3903bcb9b47b42b2f73efc6fa42b1b2d4f06bee0
Adam <a.kubicki1992@gmail.com>
2015-08-14 16:27:42 +0200

    users fetched

--- app/assets/javascripts/angular/controllers/stats_controller.js
+++ app/assets/javascripts/angular/controllers/stats_controller.js
@@ -1,2 +1,17 @@
 angular.module('app').controller('statsCtrl', ['$scope', 'FlashMessage', function($scope, FlashMessage) {
+
+  $scope.fetchUsers = function() {
+    query = new Parse.Query(Parse.User)
+    query.find({}).then(function(users) {
+      $scope.users = users;
+      var roleQuery = new Parse.Query("RoleInfo");
+      return query.find({})
+    }, function(errors) {}).then(function(roleInfo) {
+      $scope.$apply();
+    }, function(errors) {
+      $scope.$apply();
+    })
+  }
+  $scope.fetchUsers();
+
 }]);
