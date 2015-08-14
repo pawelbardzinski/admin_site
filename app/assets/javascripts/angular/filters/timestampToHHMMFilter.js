d7f1b3478db4786ec52a802f1326e0f1df441b8d
Adam <a.kubicki1992@gmail.com>
2015-08-14 19:12:42 +0200

    gmt updated

--- app/assets/javascripts/angular/filters/timestampToHHMMFilter.js
+++ app/assets/javascripts/angular/filters/timestampToHHMMFilter.js
@@ -1,7 +1,7 @@
 app.filter('timestampToHHMMFilter', ['$filter', function($filter) {
   return function(arrayOfTimestamps) {
     if(arrayOfTimestamps){
-      return _.map(arrayOfTimestamps, function(timestamp){   return $filter('date')(timestamp * 1000, 'HH:mm', 'Z') })
+      return _.map(arrayOfTimestamps, function(timestamp){   return $filter('date')(timestamp * 1000, 'HH:mm', 'GMT') })
     }else{
       return arrayOfTimestamps
     }
